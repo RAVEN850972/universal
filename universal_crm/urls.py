c920fee408ddf7ad42477cedf7755fa8c3ad1f0d
RAVEN850972 <timmendax@gmail.com>
2025-06-12 13:32:29 +0500

    Update urls.py

--- universal_crm/urls.py
+++ universal_crm/urls.py
@@ -1,3 +1,4 @@
+# universal_crm/urls.py (обновленная версия)
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -7,8 +8,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('crm.urls')),
     path('api/', include('crm.api_urls')),
+    path('', include('crm.modal_urls')),  # Добавляем модальные URL
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
